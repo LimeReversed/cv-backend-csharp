99c455f0b11efe871430100adccfc5f89e95ec76
Lime Fortytwo <lime42@pm.me>
2025-10-05 15:19:13 +0200

    Added transaction for several of getters in a row

--- Controllers/ExperienceController.cs
+++ Controllers/ExperienceController.cs
@@ -19,7 +19,7 @@ public class ExperienceController : ControllerBase
     public List<Experience> Get()
     {
         var experience = ExperienceRepository.GetAll();
-        return PackageExperience(experience);
+        return RepositoryHelpers.FillExperience(experience);
 
     }
 
@@ -27,7 +27,7 @@ public class ExperienceController : ControllerBase
     public List<Experience> GetJobs()
     {
         var experience = ExperienceRepository.GetJobs();
-        return PackageExperience(experience);
+        return RepositoryHelpers.FillExperience(experience);
         
     }
 
@@ -35,7 +35,7 @@ public class ExperienceController : ControllerBase
     public List<Experience> GetEducation()
     {
         var experience = ExperienceRepository.GetEducation();
-        return PackageExperience(experience);
+        return RepositoryHelpers.FillExperience(experience);
 
     }
 
@@ -43,41 +43,7 @@ public class ExperienceController : ControllerBase
     public List<Experience> GetHobbies()
     {
         var experience = ExperienceRepository.GetHobbies();
-        return PackageExperience(experience);
+        return RepositoryHelpers.FillExperience(experience);
 
-    }
-
-    private List<Experience> PackageExperience(List<Experience> experience)
-    {
-        /*
-         * I debated whether to add a transaction for these calls to the database. 
-         * The reason being that experience, projects and image paths are all connected, and what if one changes after getting one but before getting the other?
-         * There is a risk for dirty reads. I could start a transaction here, but a controller or a repository call should not care about the implementation of the storage. 
-         * Therefore I can only have code that is connected to the sqlite database inside of repositories. I could call everything I need inside of the ExperienceRepository, but then
-         * I would repeat a lot of code that is in the other repositories. So Repository pattern seems to cause issues with transactions together with DRY (Don't repeat yourself).
-         */
-
-        var experienceIds = experience.Select(x => x.Id).ToList();
-        
-        var projectsByExperienceIds = ProjectRepository.GetByExperienceIds(experienceIds);
-        var tagsByExperienceIds = TagRepository.GetByExperienceIds(experienceIds);
-
-        var projectIds = projectsByExperienceIds.SelectMany(keyValuePair => keyValuePair.Value).Select(project => project.Id).ToList();
-        var imagePathsByProjectIds = ImagePathRepository.GetByProjectIds(projectIds);
-
-        foreach (Experience ex in experience)
-        {
-            // Some experiences don't have projects, so some experienceIDs will not be in represented in projectsByExperienceIds.
-            // So we first need to check if the projectsByExperienceIds dictionary contains that experienceId. 
-            if (projectsByExperienceIds.ContainsKey(ex.Id)) { ex.Projects = projectsByExperienceIds[ex.Id]; }
-            if (tagsByExperienceIds.ContainsKey(ex.Id)) { ex.Tags = tagsByExperienceIds[ex.Id]; }
-
-            foreach (Project project in ex.Projects)
-            {
-                if (imagePathsByProjectIds.ContainsKey(project.Id)) { project.Imagepaths = imagePathsByProjectIds[project.Id]; }
-            }
-        }
-
-        return experience;
     }
 }
---
+++ Repository/RepositoryHelpers.cs
@@ -0,0 +1,45 @@
+using BackendCSharp.Models;
+using Microsoft.Data.Sqlite;
+using System.Diagnostics;
+
+namespace BackendCSharp.Repositories;
+
+public static class RepositoryHelpers
+{
+    public static List<Experience> FillExperience(List<Experience> experience)
+    {
+        using var connection = new SqliteConnection(@"Data Source = E:\Programs\CV\backend-c#\Database\ResumeDatabase.db3");
+        
+        try
+        {
+            var experienceIds = experience.Select(x => x.Id).ToList();
+
+            connection.Open();
+            using var transaction = connection.BeginTransaction();
+            var projectsByExperienceIds = ProjectRepository.GetByExperienceIds(experienceIds);
+            var tagsByExperienceIds = TagRepository.GetByExperienceIds(experienceIds);
+
+            var projectIds = projectsByExperienceIds.SelectMany(keyValuePair => keyValuePair.Value).Select(project => project.Id).ToList();
+            var imagePathsByProjectIds = ImagePathRepository.GetByProjectIds(projectIds);
+
+            foreach (Experience ex in experience)
+            {
+                // Some experiences don't have projects, so some experienceIDs will not be in represented in projectsByExperienceIds.
+                // So we first need to check if the projectsByExperienceIds dictionary contains that experienceId. 
+                if (projectsByExperienceIds.ContainsKey(ex.Id)) { ex.Projects = projectsByExperienceIds[ex.Id]; }
+                if (tagsByExperienceIds.ContainsKey(ex.Id)) { ex.Tags = tagsByExperienceIds[ex.Id]; }
+
+                foreach (Project project in ex.Projects)
+                {
+                    if (imagePathsByProjectIds.ContainsKey(project.Id)) { project.Imagepaths = imagePathsByProjectIds[project.Id]; }
+                }
+            }
+        }
+        catch (SqliteException e)
+        {
+            Debug.WriteLine(e.Message);
+        }
+
+        return experience;
+    }
+}
