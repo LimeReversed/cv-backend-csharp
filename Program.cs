0cd23d0329895695f37595dfc3761d8d8043bd89
Lime Reversed <limereversed@pm.me>
2026-02-23 21:22:16 +0100

    Went back to before AI change to see if the changes were needed.

--- Program.cs
+++ Program.cs
@@ -18,21 +18,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          // Allow the frontend origin(s) you serve over HTTPS. If nginx is proxying
-                          // the public host to this app the browser will talk to the same origin
-                          // (https://emilrundberg.se) so CORS won't be required. Keep these entries
-                          // to allow direct calls during development.
-                          policy.WithOrigins(
-                              "http://localhost:5173",
-                              "http://92.33.158.78",
-                              "http://emilrundberg.se",
-                              "http://www.emilrundberg.se",
-                              "https://emilrundberg.se",
-                              "https://www.emilrundberg.se",
-                              "http://192.168.10.174")
-                                .AllowAnyHeader()
-                                .AllowAnyMethod()
-                                .AllowCredentials();
+                          policy.WithOrigins("http://localhost:5173", "http://92.33.158.78", "http://emilrundberg.se", "http://www.emilrundberg.se", "https://www.emilrundberg.se", "http://192.168.10.174");
                       });
 });
 
